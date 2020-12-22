eba1eed34662b8020ee4e91f330d6f14e25e0955
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2020-12-22 16:55:47 +0700

    Fix certificate thumbprint repreival

--- MakeCert/thumbprint.ps1
+++ MakeCert/thumbprint.ps1
@@ -3,13 +3,13 @@
 
 write-host "CN=VirtualBoxSHA1"
 $getThumb1 = Get-ChildItem -path cert:\LocalMachine\Root | where { $_.Subject -match "CN\=VirtualBoxSHA1" }
-$getThumb1.thumbprint
+$getThumb1.Thumbprint
 
 write-host "CN=VirtualBoxSHA256"
 $getThumb256 = Get-ChildItem -path cert:\LocalMachine\Root | where { $_.Subject -match "CN\=VirtualBoxSHA256" }
-$getThumb256.thumbprint
+$getThumb256.Thumbprint
 
-$text = "VBOX_CERTIFICATE_FINGERPRINT := {0}`r`nVBOX_CERTIFICATE_SHA2_FINGERPRINT := {1}`r`n" -f $getThumb1.thumbprint, $getThumb256.thumbprint
+$text = "VBOX_CERTIFICATE_FINGERPRINT := {0}`r`nVBOX_CERTIFICATE_SHA2_FINGERPRINT := {1}`r`n" -f $getThumb1.Thumbprint, $getThumb256.Thumbprint
 $text | Set-Content "Certificates.kmk"
 
 Write-Host "`r`nThumbprint retreival successful. SHA1 and SHA256 thumbprints are written in Certificates.kmk file. Just add them to your VirtualBox local config or replace old values`r`n"
